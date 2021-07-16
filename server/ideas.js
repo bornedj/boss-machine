5a7fb119f7df56579d191b33ca58114deccd4b8a
bornedj <bornedj@miamioh.edu>
2021-07-16 12:35:30 -0400

    working post to ideas:

--- server/ideas.js
+++ server/ideas.js
@@ -23,4 +23,22 @@ ideasRouter.get('/:ideaId', (req, res, next) => {
     res.send(req.idea)
 })
 
+// post new idea
+ideasRouter.post('/', (req, res, next) => {
+    //validate body data
+    if (typeof(req.body.name) === 'string' && typeof(req.body.description) === 'string' 
+    && typeof(req.body.numWeeks) === 'number' && typeof(req.body.weeklyRevenue) === 'number')
+    {
+        const ideaInstance = {
+            name: req.body.name,
+            description: req.body.description,
+            numWeeks: req.body.numWeeks,
+            weeklyRevenue: req.body.weeklyRevenue 
+        }
+        const newIdea = addToDatabase('ideas', ideaInstance)
+        return res.status(201).send(newIdea)
+    }
+    res.status(500).send('error in idea typing')
+})
+
 module.exports = ideasRouter;
