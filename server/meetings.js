d9feb6a37e555ce88607186d83cc26792973d284
bornedj <bornedj@miamioh.edu>
2021-07-16 12:56:54 -0400

    finished the delete

--- server/meetings.js
+++ server/meetings.js
@@ -1,5 +1,5 @@
 const meetingsRouter = require('express').Router();
-const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId, createMeeting } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId, createMeeting, deleteAllFromDatabase } = require('./db');
 
 // get all
 meetingsRouter.get('/', (req, res, next) => {
@@ -11,6 +11,12 @@ meetingsRouter.post('/', (req, res, next) => {
     const newMeeting = createMeeting();
     addToDatabase('meetings', newMeeting);
     res.status(201).send(newMeeting);
+})
+
+// delete all from the database
+meetingsRouter.delete('/', (req, res, next) => {
+    deleteAllFromDatabase('meetings');
+    res.status(204).send('all meetings have been delted')
 })
 
 module.exports = meetingsRouter;
