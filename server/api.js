0db22c6451ae3bad9772c9b9b27d08722bcea1be
bornedj <bornedj@miamioh.edu>
2021-07-16 12:19:53 -0400

    code split the apiRouter

--- server/api.js
+++ server/api.js
@@ -1,65 +1,66 @@
 const express = require('express');
-const { getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteAllFromDatabase } = require('./db');
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteAllFromDatabase } = require('./db');
+const minionRouter = require('./minions');
 const apiRouter = express.Router();
-const { getAllFromDatabase } = require('./db');
 
+apiRouter.use('/minions', minionRouter)
 // working on the minions route 
 // params for minion id
-apiRouter.param('minionId', (req, res, next, id) => {
-    const minion =  getFromDatabaseById('minions', id);
-    if (!minion) {
-        res.status(404).send('Minion not found');
-    }
+// apiRouter.param('minionId', (req, res, next, id) => {
+//     const minion =  getFromDatabaseById('minions', id);
+//     if (!minion) {
+//         res.status(404).send('Minion not found');
+//     }
 
-    req.minion = minion;
-    next();
-})
+//     req.minion = minion;
+//     next();
+// })
 
-//get all
-apiRouter.get('/minions', (req, res, next) => {
-  res.send(getAllFromDatabase('minions'))
-})
+// //get all
+// apiRouter.get('/minions', (req, res, next) => {
+//   res.send(getAllFromDatabase('minions'))
+// })
 
-//get minion by id
-apiRouter.get('/minions/:minionId', (req, res, next) => {
-    res.send(req.minion);
-})
+// //get minion by id
+// apiRouter.get('/minions/:minionId', (req, res, next) => {
+//     res.send(req.minion);
+// })
 
-// create a new minion
-apiRouter.post('/minions', (req, res, next) => {
-    if (typeof(req.body.name) === 'string' && typeof(req.body.title) === 'string' 
-    && typeof(req.body.salary) === 'number') {
-        const minionInstance = {
-            name: req.body.name,
-            title: req.body.title,
-            salary: req.body.salary
-        }
-        const newMinion = addToDatabase('minions', minionInstance)
-        return res.status(201).send(newMinion)
-    }
+// // create a new minion
+// apiRouter.post('/minions', (req, res, next) => {
+//     if (typeof(req.body.name) === 'string' && typeof(req.body.title) === 'string' 
+//     && typeof(req.body.salary) === 'number') {
+//         const minionInstance = {
+//             name: req.body.name,
+//             title: req.body.title,
+//             salary: req.body.salary
+//         }
+//         const newMinion = addToDatabase('minions', minionInstance)
+//         return res.status(201).send(newMinion)
+//     }
 
-    res.status(500).send('error in minion typing')
-})
+//     res.status(500).send('error in minion typing')
+// })
 
-// updating a minion by id
-apiRouter.put('/minions/:minionId', (req, res, next) => {
-    if (req.minion) {
-        const updatedMinion = req.minion;
-        updatedMinion.name = req.body.name;
-        updatedMinion.title = req.body.title;
-        updatedMinion.salary = req.body.salary;
+// // updating a minion by id
+// apiRouter.put('/minions/:minionId', (req, res, next) => {
+//     if (req.minion) {
+//         const updatedMinion = req.minion;
+//         updatedMinion.name = req.body.name;
+//         updatedMinion.title = req.body.title;
+//         updatedMinion.salary = req.body.salary;
 
-        const returnMinion = updateInstanceInDatabase('minions', updatedMinion);
-        res.send(returnMinion);
-    }
-})
+//         const returnMinion = updateInstanceInDatabase('minions', updatedMinion);
+//         res.send(returnMinion);
+//     }
+// })
 
-// deleting a minion
-apiRouter.delete('/minions/:minionId', (req, res, next) => {
-    if (req.minion) {
-        deleteAllFromDatabase('minions', req.minion.id);
-        res.status(204).send('minion deleted')
-    }
-})
+// // deleting a minion
+// apiRouter.delete('/minions/:minionId', (req, res, next) => {
+//     if (req.minion) {
+//         deleteAllFromDatabase('minions', req.minion.id);
+//         res.status(204).send('minion deleted')
+//     }
+// })
 
 module.exports = apiRouter;
---
+++ server/minions.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { getAllFromDatabase, getFromDatabaseById, addToDatabase, updateInstanceInDatabase, deleteAllFromDatabase } = require('./db');
+const minionRouter = express.Router();
+
+minionRouter.param('minionId', (req, res, next, id) => {
+    const minion =  getFromDatabaseById('minions', id);
+    if (!minion) {
+        res.status(404).send('Minion not found');
+    }
+
+    req.minion = minion;
+    next();
+})
+
+//get all
+minionRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('minions'))
+})
+
+//get minion by id
+minionRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+})
+
+// create a new minion
+minionRouter.post('/', (req, res, next) => {
+    if (typeof(req.body.name) === 'string' && typeof(req.body.title) === 'string' 
+    && typeof(req.body.salary) === 'number') {
+        const minionInstance = {
+            name: req.body.name,
+            title: req.body.title,
+            salary: req.body.salary
+        }
+        const newMinion = addToDatabase('minions', minionInstance)
+        return res.status(201).send(newMinion)
+    }
+
+    res.status(500).send('error in minion typing')
+})
+
+// updating a minion by id
+minionRouter.put('/:minionId', (req, res, next) => {
+    if (req.minion) {
+        const updatedMinion = req.minion;
+        updatedMinion.name = req.body.name;
+        updatedMinion.title = req.body.title;
+        updatedMinion.salary = req.body.salary;
+
+        const returnMinion = updateInstanceInDatabase('minions', updatedMinion);
+        res.send(returnMinion);
+    }
+})
+
+// deleting a minion
+minionRouter.delete('/:minionId', (req, res, next) => {
+    if (req.minion) {
+        deleteAllFromDatabase('minions', req.minion.id);
+        res.status(204).send('minion deleted')
+    }
+})
+
+module.exports = minionRouter;
